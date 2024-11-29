760c082d63b848905ecc6ebaf5df1ec96a7a02fb
Jihyun3478 <leejihyun3478@gmail.com>
2024-11-29 16:30:33 +0900

    feat : 프로젝트 초기 세팅

---
+++ src/main/java/com/example/myyouthpolicy/MyYouthPolicyApplication.java
@@ -0,0 +1,13 @@
+package com.example.myyouthpolicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyYouthPolicyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyYouthPolicyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myyouthpolicy/MyYouthPolicyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myyouthpolicy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyYouthPolicyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
